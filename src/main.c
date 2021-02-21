d843846504c7439e3a64b21e1a9cdb9226096f50
Tim Boldt <tim.boldt@gmail.com>
2021-02-21 12:54:06 -0800

    Fancier blinky

--- src/main.c
+++ src/main.c
@@ -14,18 +14,24 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
 
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
 #define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#else
-/* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
-#endif
+#define LED1	DT_GPIO_LABEL(LED1_NODE, gpios)
+#define LED2	DT_GPIO_LABEL(LED2_NODE, gpios)
+#define LED3	DT_GPIO_LABEL(LED3_NODE, gpios)
+
+#define PIN0	DT_GPIO_PIN(LED0_NODE, gpios)
+#define PIN1	DT_GPIO_PIN(LED1_NODE, gpios)
+#define PIN2	DT_GPIO_PIN(LED2_NODE, gpios)
+#define PIN3	DT_GPIO_PIN(LED3_NODE, gpios)
+
+#define FLAGS0	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#define FLAGS1	DT_GPIO_FLAGS(LED1_NODE, gpios)
+#define FLAGS2	DT_GPIO_FLAGS(LED2_NODE, gpios)
+#define FLAGS3	DT_GPIO_FLAGS(LED3_NODE, gpios)
 
 void main(void)
 {
@@ -38,14 +44,32 @@ void main(void)
 		return;
 	}
 
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	ret = gpio_pin_configure(dev, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
+	if (ret < 0) {
+		return;
+	}
+	ret = gpio_pin_configure(dev, PIN1, GPIO_OUTPUT_ACTIVE | FLAGS1);
+	if (ret < 0) {
+		return;
+	}
+	ret = gpio_pin_configure(dev, PIN2, GPIO_OUTPUT_ACTIVE | FLAGS2);
+	if (ret < 0) {
+		return;
+	}
+	ret = gpio_pin_configure(dev, PIN3, GPIO_OUTPUT_ACTIVE | FLAGS3);
 	if (ret < 0) {
 		return;
 	}
 
 	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
+		gpio_pin_set(dev, PIN0, (int)led_is_on);
 		k_msleep(SLEEP_TIME_MS);
+		gpio_pin_set(dev, PIN1, (int)led_is_on);
+		k_msleep(SLEEP_TIME_MS);
+		gpio_pin_set(dev, PIN2, (int)led_is_on);
+		k_msleep(SLEEP_TIME_MS);
+		gpio_pin_set(dev, PIN3, (int)led_is_on);
+		k_msleep(SLEEP_TIME_MS);
+		led_is_on = !led_is_on;
 	}
 }
